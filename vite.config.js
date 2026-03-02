cac8e8f3fbde1ca08eb99cd055476f78e0162387
Tobiáš Sirotňák <tobiassirotnak@gmail.com>
2026-03-02 17:37:24 +0100

    fixed build yaml

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/winet/' : '/',
+export default defineConfig({
   plugins: [react()],
-}))
+})
