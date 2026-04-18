5ed100fb0fa10ed66ea873ffdd90f6684c8bf8a8
Tobiáš Sirotňák <tobiassirotnak@gmail.com>
2026-04-18 15:16:52 +0200

    Fixed base url

--- winet.client/src/main.tsx
+++ winet.client/src/main.tsx
@@ -7,7 +7,10 @@ import './index.css'
 import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ 
+  routeTree,
+  basepath: import.meta.env.BASE_URL 
+})
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
